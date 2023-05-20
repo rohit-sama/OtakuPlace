61bbfe62d7bd6878bcd73c5f5fc531ec3b4680be
rohit-sama <rohit9804singh@gmail.com>
2023-05-20 14:40:05 +0530

    some changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,7 +20,8 @@ function App() {
       <Route index element = {<IndexPage />}/>
       <Route path='/login' element = {<LoginPage />}/>
       <Route path='/register' element = {<RegisterPage/>}/>
-      <Route path='/account' element = {<Account/>}/>
+      <Route path='/account/:subpage?' element = {<Account/>}/>
+      
     </Route>
     </Routes>
     </UserContextProvider>
--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,16 +1,51 @@
 import { useContext } from "react";
 import { UserContext } from "../UserContext";
-import { Navigate } from "react-router-dom";
-
+import { Link, Navigate, useParams } from "react-router-dom";
+import img from "../assets/images.jpeg"
 
 export default function IndexPage() { 
     const {ready,user} = useContext(UserContext);
 
+    let {subpage} = useParams();
+    if(subpage == undefined) {
+      subpage = 'profile';
+    }
     if(ready && !user) {
         return <Navigate to = {'/login'} />
     }
 
+    
+
+    function LinkClasses (type=null) {
+      let classes = 'py-2 px-4';
+      if (type === subpage ) {
+      classes+= ' bg-primary text-white rounded-full';
+    }
+    return classes;
+    }
+    
+
   return (
-   <div>name = {user?.name}</div>
+    <div>
+   <nav className="w-full flex mt-8 justify-center gap-4">
+    <Link className={LinkClasses('profile')} to ={'/account'} >My Profile</Link>
+    <Link className={LinkClasses('bookings')} to ={'/account/bookings'} >My Bookings</Link>
+    <Link className={LinkClasses('places')}  to ={'/account/places'} >My Accomodations</Link>
+   </nav>
+   {subpage === 'profile' && (
+    <div className=" p-6 rounded-md mt-40 flex justify-center gap-4">
+         <div><img className="rounded-md" src = {img} alt=""/></div>
+         <div className="text-center mt-10">
+         Logged in as <br />
+         <div className="text-2xl ">
+         Name : {user.name} <br />
+         Email ID : {user.email}<br />
+         </div>
+         <button className="bg-primary p-2 mt-2 rounded-md">Logout</button>
+         </div>
+    </div>
+   )}
+   </div>
+   
   );
 }
